f73c221bf15230b70c41a985577a89c7d4ee6069
datumcoder <datumcoder@gmail.com>
2022-03-29 20:30:11 +0200

    First commit added jsx and sass to the project.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Fourblocks from "./components/Fourblocks";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Fourblocks />
+    </div>
+  );
+}
+
+export default App;
