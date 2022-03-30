e2f28e18fbab2f8ef941ff720dc4b236762697c4
datumcoder <datumcoder@gmail.com>
2022-03-30 15:47:24 +0200

    Updated jsx and scss files

--- src/components/Fourblocks.jsx
+++ src/components/Fourblocks.jsx
@@ -1,52 +1,59 @@
 function Fourblocks() {
   return (
     <div className="container">
-      <div className="card_1">
+      <div className="box 1">
         <div className="content">
           <div className="card-title-top">
             <p className="main-title">Metric/Imperial unit conversion</p>
             <p className="conversion">20</p>
           </div>
-
+          <div className="box_in A">
           <div className="length">Length (Meter/Feet)</div>
           <div className="vol">Volume (Liters/Gallons)</div>
-          <div className="mass">Mass (Kilograms/Pounds)</div>
+            <div className="mass">Mass (Kilograms/Pounds)</div>
+            </div>
         </div>
       </div>
-      <div className="card_2">
+      <div className="box 2">
         <div className="content">
           <div className="card-title-top">
             <p className="main-title">Metric/Imperial unit conversion</p>
             <p className="conversion">20</p>
           </div>
+            <div className="box_in b">
           <div className="length">Length (Meter/Feet)</div>
           <div className="vol">Volume (Liters/Gallons)</div>
-          <div className="mass">Mass (Kilograms/Pounds)</div>
+            <div className="mass">Mass (Kilograms/Pounds)</div>
+          </div>
         </div>
       </div>
-      <div className="card_3">
+      <div className="box 3">
         <div className="content">
-          <div className="card-title-bottom">
+          <div className="card-title-top">
             <p className="main-title">Metric/Imperial unit conversion</p>
             <p className="conversion">20</p>
           </div>
+          <div className="box_in c">
           <div className="length">Length (Meter/Feet)</div>
           <div className="vol">Volume (Liters/Gallons)</div>
-          <div className="mass">Mass (Kilograms/Pounds)</div>
+            <div className="mass">Mass (Kilograms/Pounds)</div>
+            </div>
         </div>
       </div>
-      <div className="card_4">
+      <div className="box 4">
         <div className="content">
-          <div className="card-title-bottom">
+          <div className="card-title-top">
             <p className="main-title">Metric/Imperial unit conversion</p>
             <p className="conversion">20</p>
           </div>
+          <div className="box_in D">
           <div className="length">Length (Meter/Feet)</div>
           <div className="vol">Volume (Liters/Gallons)</div>
-          <div className="mass">Mass (Kilograms/Pounds)</div>
+            <div className="mass">Mass (Kilograms/Pounds)</div>
+          </div>
         </div>
       </div>
-    </div>
+ </div>
   );
 }
 
