31f3d66f049baf027206e0361a818a9e92000c70
Rafal <r.michniuk@gmail.com>
2022-05-25 18:59:00 +0200

    refactor - object deconstruction

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import Card from "./components/Card/Card";
 import "./App.scss";
 import Container from "./components/Container/Container";
 
-const cardData = ["dark", "dark", "white", "white"];
-const variants = cardData.map((data) => <Card variant={data} />);
-
 const App = () => (
   <div className="App">
-    <Container>{variants}</Container>
+    <Container>
+      <Card variant={"dark"} />
+    </Container>
   </div>
 );
 
 export default App;
-
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Container.scss";
 
-const Container = (props) => <div className="container">{props.children}</div>;
+const Container = ({ children }) => <div className="container">{children}</div>;
 
 export default Container;
